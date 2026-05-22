1a82d0b2a34915fe637bad36bd6f8ec4a3b97c3b
Bob Weigel <Robert.S.Weigel@gmail.com>
2026-05-22 14:18:50 -0400

    Check existence of G2DRF directory
    Add check for existence of G2DRF directory before listing OBS directories.

--- drf.py
+++ drf.py
@@ -657,7 +657,12 @@ def _subset_sample_dirs(station_id, station_dir, n=None, first_last=False, start
   if len(obs_dirs) == 0:
     logger.info(station_id, f"  No 'OBS*' directories found in {station_dir}. Trying subdirectory 'G2DRF' for station {station_id}.")
     station_dir = os.path.join(station_dir, "G2DRF")
-    obs_dirs = [d for d in _listdir(station_dir) if d.startswith('OBS')]
+    if os.path.exists(station_dir)
+      obs_dirs = [d for d in _listdir(station_dir) if d.startswith('OBS')]
+      if len(obs_dirs) == 0:
+        return []
+    else:
+      return []
 
   pat = re.compile(r'^OBS(\d{4}-\d{2}-\d{2}T\d{2}-\d{2})')
 
